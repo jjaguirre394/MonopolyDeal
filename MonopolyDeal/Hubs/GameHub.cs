8e57e395d71d51a1ec868f90c226e66bfad7f932
jjaguirre394 <jjaguirre394@gmail.com>
2020-05-09 21:18:40 -0700

    Added working lobby test code

--- MonopolyDeal/Hubs/GameHub.cs
+++ MonopolyDeal/Hubs/GameHub.cs
@@ -5,6 +5,14 @@ namespace MonopolyDeal.Hubs
 {
     public class GameHub : Hub
     {
+        public async Task JoinRoom(string roomName)
+        {
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+        }
+        public async Task LeaveRoom(string roomName)
+        {
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
+        }
         public async Task SendMessage(string user, string message)
         {
             await Clients.All.SendAsync("ReceiveMessage", user, message);
