0d49df9b3976ab618f4054b85d66de6fb53fe4bf
Juan Aguirre <jjaguirre394@gmail.com>
2020-05-25 17:17:55 -0700

    removing unnecessary commented code from Hub

--- MonopolyDeal/Hubs/GameHub.cs
+++ MonopolyDeal/Hubs/GameHub.cs
@@ -5,33 +5,21 @@ using System.Threading.Tasks;
 namespace MonopolyDeal.Hubs
 {
     public class GameHub : Hub
-    {
-        //private HashSet<string> Rooms = new HashSet<string>(); 
+    { 
         public async Task CreateRoom(string roomName, string user)
         {
-            //if (!Rooms.Contains(roomName))
-            //{
-                //Rooms.Add(roomName);
-                await JoinRoom(roomName, user);
-            //}
+            await JoinRoom(roomName, user);
         }
 
         public async Task JoinRoom(string roomName, string user)
         {
-            //if (Rooms.Contains(roomName))
-            //{
-                await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
-                await Clients.Others.SendAsync("UserJoined", user);
-            //}
+            await Groups.AddToGroupAsync(Context.ConnectionId, roomName);
+            await Clients.Others.SendAsync("UserJoined", user);
         }
 
         public async Task LeaveRoom(string roomName)
         {
-            //if (Rooms.Contains(roomName))
-            //{
-                await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
-                //Rooms.Remove(roomName);
-            //}
+            await Groups.RemoveFromGroupAsync(Context.ConnectionId, roomName);
         }
         
         public async Task SendState(string user, string state)
@@ -41,7 +29,6 @@ namespace MonopolyDeal.Hubs
 
         public async Task StartGame()
         {
-            //Initialize the game state and send it to all clients
             await Clients.Others.SendAsync("ReceiveStart");
         }
     }
